d3e16bd1ffaa925de33a5032957607eb7dce9ee6
ArnavKumar270 <arnavkumar272005@gmail.com>
2025-02-28 13:57:22 -0700

    committed

--- src/utils/deployPools.ts
+++ src/utils/deployPools.ts
@@ -1,17 +1,17 @@
 import {
-	createPublicClient,
-	createWalletClient,
-	http,
-	erc20Abi,
-	type PrivateKeyAccount,
+    createPublicClient,
+    createWalletClient,
+    http,
+    erc20Abi,
+    type PrivateKeyAccount,
 } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
 import {
-	DEADLINE,
-	PRIVATE_KEY,
-	PUBLIC_KEY,
-	chainIDToRPCUrls,
-	chainIDtoChain,
+    DEADLINE,
+    PRIVATE_KEY,
+    PUBLIC_KEY,
+    chainIDToRPCUrls,
+    chainIDtoChain,
 } from '../config/config';
 import type { WalletClient, PublicClient } from 'viem';
 import { ROUTER_V2_ABI } from '../constants/abi/ABIs';
@@ -19,71 +19,70 @@ import { UNISWAP_V2_ROUTER_ADDRESSES } from '../constants/addresses';
 import { publicActionsL2, walletActionsL2 } from '@eth-optimism/viem';
 
 export const addLiquidity = async (
-	amountEth: bigint,
-	token: `0x${string}`,
-	amountToken: bigint,
-	chainId: number,
-	account: PrivateKeyAccount,
+    amountEth: bigint,
+    token: `0x${string}`,
+    amountToken: bigint,
+    chainId: number,
+    account: PrivateKeyAccount,
 ) => {
-	const walletClient = createWalletClient({
-		transport: http(chainIDToRPCUrls.get(chainId) as string),
-		chain: chainIDtoChain.get(chainId),
-	}).extend(walletActionsL2());
+    const walletClient = createWalletClient({
+        transport: http(chainIDToRPCUrls.get(chainId) as string),
+        chain: chainIDtoChain.get(chainId),
+    }).extend(walletActionsL2());
 
-	const publicClient = createPublicClient({
-		transport: http(chainIDToRPCUrls.get(chainId) as string),
-		chain: chainIDtoChain.get(chainId),
-	}).extend(publicActionsL2());
+    const publicClient = createPublicClient({
+        transport: http(chainIDToRPCUrls.get(chainId) as string),
+        chain: chainIDtoChain.get(chainId),
+    }).extend(publicActionsL2());
 
-	try {
-		// Approve add liquidity
-		const routerV2Address = UNISWAP_V2_ROUTER_ADDRESSES.get(walletClient.chain?.id as number);
+    try {
+        // Approve add liquidity
+        const routerV2Address = UNISWAP_V2_ROUTER_ADDRESSES.get(walletClient.chain?.id as number);
 
-		if (!routerV2Address) {
-			return;
-		}
+        if (!routerV2Address) {
+            return;
+        }
 
-		const approveAddLiquidityHash = await walletClient.writeContract({
-			abi: erc20Abi,
-			address: token as `0x${string}`,
-			functionName: 'approve',
-			args: [
-				routerV2Address,
-				115792089237316195423570985008687907853269984665640564039457584007913129639935n,
-			],
-			nonce: 1,
-			gas: 100000n,
-			chain: walletClient.chain,
-			account: account,
-		});
+        const approveAddLiquidityHash = await walletClient.writeContract({
+            abi: erc20Abi,
+            address: token as `0x${string}`,
+            functionName: 'approve',
+            args: [
+                routerV2Address,
+                115792089237316195423570985008687907853269984665640564039457584007913129639935n,
+            ],
+            gas: 100000n,
+            chain: walletClient.chain,
+            account: account,
+        });
 
-		const approveAddLiquidityReceipt = await publicClient.waitForTransactionReceipt({
-			hash: approveAddLiquidityHash,
-		});
+        const approveAddLiquidityReceipt = await publicClient.waitForTransactionReceipt({
+            hash: approveAddLiquidityHash,
+        });
 
-		const { request, result } = await publicClient.simulateContract({
-			account: PUBLIC_KEY,
-			address: routerV2Address,
-			abi: ROUTER_V2_ABI,
-			functionName: 'addLiquidityETH',
-			args: [
-				token as `0x${string}`,
-				amountToken,
-				amountToken,
-				amountEth,
-				PUBLIC_KEY,
-				BigInt(DEADLINE),
-			],
-			value: amountEth,
-			// nonce: 2,
-			gas: 3000000n,
-		});
+        const { request, result } = await publicClient.simulateContract({
+            account: PUBLIC_KEY,
+            address: routerV2Address,
+            abi: ROUTER_V2_ABI,
+            functionName: 'addLiquidityETH',
+            args: [
+                token as `0x${string}`,
+                amountToken,
+                amountToken,
+                amountEth,
+                PUBLIC_KEY,
+                BigInt(DEADLINE),
+            ],
+            value: amountEth,
+            // nonce: 2,
+            gas: 3000000n,
+        });
 
-		const liquidityBalance = result;
+        const liquidityBalance = result;
 
-		const addLiquidityHash = await walletClient.writeContract(request);
-	} catch (error: any) {
-		console.error('Error adding liquidity:', error);
-		return null;
-	}
+        const addLiquidityHash = await walletClient.writeContract(request);
+    } catch (error: any) {
+        console.error('Error adding liquidity:', error);
+        return null;
+    }
 };
--- src/utils/uniswapv2.ts
+++ src/utils/uniswapv2.ts
@@ -1,52 +1,52 @@
 // ADD LIQUIDITY FUNCTION
 
 import {
-	type WalletClient,
-	getCreate2Address,
-	keccak256,
-	encodePacked,
-	type PrivateKeyAccount,
+    type WalletClient,
+    getCreate2Address,
+    keccak256,
+    encodePacked,
+    type PrivateKeyAccount,
 } from 'viem';
 import { ROUTER_V2_ABI } from '../constants/abi/ABIs';
 import { DEADLINE } from '../config/config';
 import {
-	UNISWAP_V2_ROUTER_ADDRESSES,
-	WETH_ADDRESSES,
-	UNISWAP_V2_FACTORY_ADDRESSES,
-	INIT_CODE_HASHES,
+    UNISWAP_V2_ROUTER_ADDRESSES,
+    WETH_ADDRESSES,
+    UNISWAP_V2_FACTORY_ADDRESSES,
+    INIT_CODE_HASHES,
 } from '../constants/addresses';
 
 export const getAmountOut = (
-	amountIn: bigint,
-	reserveIn: bigint,
-	reserveOut: bigint,
-	fees: number,
+    amountIn: bigint,
+    reserveIn: bigint,
+    reserveOut: bigint,
+    fees: number,
 ): bigint => {
-	const feesBips = fees * 100;
-	const amountInWithFee = amountIn * BigInt(1000 - feesBips);
-	const numerator = amountInWithFee * reserveOut;
-	const denominator = reserveIn * BigInt(1000) + amountInWithFee;
-	const amountOut = numerator / denominator;
-	return amountOut;
+    const feesBips = fees * 100;
+    const amountInWithFee = amountIn * BigInt(1000 - feesBips);
+    const numerator = amountInWithFee * reserveOut;
+    const denominator = reserveIn * BigInt(1000) + amountInWithFee;
+    const amountOut = numerator / denominator;
+    return amountOut;
 };
 
 export const pairFor = (
-	tokenA: `0x${string}`,
-	tokenB: `0x${string}`,
-	chainId: number,
+    tokenA: `0x${string}`,
+    tokenB: `0x${string}`,
+    chainId: number,
 ): `0x${string}` => {
-	const tokenOrder: readonly [`0x${string}`, `0x${string}`] =
-		tokenA < tokenB ? [tokenA, tokenB] : [tokenB, tokenA];
-	const factoryAddress = UNISWAP_V2_FACTORY_ADDRESSES.get(chainId)!;
-	const initCodeHash = INIT_CODE_HASHES.get(chainId)!;
+    const tokenOrder: readonly [`0x${string}`, `0x${string}`] =
+        tokenA < tokenB ? [tokenA, tokenB] : [tokenB, tokenA];
+    const factoryAddress = UNISWAP_V2_FACTORY_ADDRESSES.get(chainId)!;
+    const initCodeHash = INIT_CODE_HASHES.get(chainId)!;
 
-	const pairAddress = getCreate2Address({
-		from: factoryAddress,
-		bytecodeHash: initCodeHash,
-		salt: keccak256(encodePacked(['address', 'address'], tokenOrder)),
-	});
+    const pairAddress = getCreate2Address({
+        from: factoryAddress,
+        bytecodeHash: initCodeHash,
+        salt: keccak256(encodePacked(['address', 'address'], tokenOrder)),
+    });
 
-	return pairAddress;
+    return pairAddress;
 };
 
 // flexibility for different chains
@@ -60,46 +60,46 @@ export const pairFor = (
  * needed for fat fingering functionality
  */
 export const buy = async (
-	walletClient: WalletClient,
-	account: PrivateKeyAccount,
-	buyAmount: bigint,
-	token: `0x${string}`,
-	chainId: number,
+    walletClient: WalletClient,
+    account: PrivateKeyAccount,
+    buyAmount: bigint,
+    token: `0x${string}`,
+    chainId: number,
 ) => {
-	const buyPath = [WETH_ADDRESSES.get(chainId)!, token];
+    const buyPath = [WETH_ADDRESSES.get(chainId)!, token];
 
-	if (!walletClient.chain) {
-		return;
-	}
+    if (!walletClient.chain) {
+        return;
+    }
 
-	const request = await walletClient.writeContract({
-		address: UNISWAP_V2_ROUTER_ADDRESSES.get(chainId)!,
-		abi: ROUTER_V2_ABI,
-		functionName: 'swapExactETHForTokens',
-		args: [1n, buyPath, account.address, BigInt(DEADLINE)],
-		value: buyAmount,
-		gas: 500000n,
-		account: account,
-		chain: walletClient.chain,
-	});
+    const request = await walletClient.writeContract({
+        address: UNISWAP_V2_ROUTER_ADDRESSES.get(chainId)!,
+        abi: ROUTER_V2_ABI,
+        functionName: 'swapExactETHForTokens',
+        args: [1n, buyPath, account.address, BigInt(DEADLINE)],
+        value: buyAmount,
+        gas: 500000n,
+        account: account,
+        chain: walletClient.chain,
+    });
 };
 
 export const sell = async (
-	walletClient: WalletClient,
-	account: PrivateKeyAccount,
-	sellAmount: bigint,
-	token: `0x${string}`,
-	chainId: number,
+    walletClient: WalletClient,
+    account: PrivateKeyAccount,
+    sellAmount: bigint,
+    token: `0x${string}`,
+    chainId: number,
 ) => {
-	const sellPath = [token, WETH_ADDRESSES.get(chainId)!];
+    const sellPath = [token, WETH_ADDRESSES.get(chainId)!];
 
-	const request = await walletClient.writeContract({
-		address: UNISWAP_V2_ROUTER_ADDRESSES.get(chainId)!,
-		abi: ROUTER_V2_ABI,
-		functionName: 'swapExactTokensForETH',
-		args: [sellAmount, 1n, sellPath, account.address, BigInt(DEADLINE)],
-		gas: 500000n,
-		account: account,
-		chain: walletClient.chain,
-	});
+    const request = await walletClient.writeContract({
+        address: UNISWAP_V2_ROUTER_ADDRESSES.get(chainId)!,
+        abi: ROUTER_V2_ABI,
+        functionName: 'swapExactTokensForETH',
+        args: [sellAmount, 1n, sellPath, account.address, BigInt(DEADLINE)],
+        gas: 500000n,
+        account: account,
+        chain: walletClient.chain,
+    });
 };
