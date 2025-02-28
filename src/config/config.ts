9625d9d370715fe8aa7b0dcf097a526412a06374
ArnavKumar270 <arnavkumar272005@gmail.com>
2025-02-28 12:52:25 -0700

    commited config changes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,21 +1,21 @@
 import { type Client, type PublicClient, type WalletClient } from 'viem';
 import {
-	http,
-	encodeFunctionData,
-	createPublicClient,
-	createWalletClient,
-	parseAbi,
-	defineChain,
-	publicActions,
-	walletActions,
-	type Chain,
+    http,
+    encodeFunctionData,
+    createPublicClient,
+    createWalletClient,
+    parseAbi,
+    defineChain,
+    publicActions,
+    walletActions,
+    type Chain,
 } from 'viem';
 import {
-	contracts,
-	publicActionsL2,
-	walletActionsL2,
-	createInteropSentL2ToL2Messages,
-	decodeRelayedL2ToL2Messages,
+    contracts,
+    publicActionsL2,
+    walletActionsL2,
+    createInteropSentL2ToL2Messages,
+    decodeRelayedL2ToL2Messages,
 } from '@eth-optimism/viem';
 
 import { privateKeyToAccount } from 'viem/accounts';
@@ -38,11 +38,11 @@ export const PUBLIC_KEY = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
 export const DEADLINE = 2000000000; // used for deadline parameter in a variety of functions
 
 export const chainIDToRPCUrls: Map<number, `https://${string}` | `http://${string}`> = new Map<
-	number,
-	`https://${string}` | `http://${string}`
+    number,
+    `https://${string}` | `http://${string}`
 >([
-	[10, 'http://127.0.0.1:9545'],
-	[8453, 'http://127.0.0.1:9546'],
+    [10, 'http://127.0.0.1:9545'],
+    [8453, 'http://127.0.0.1:9546'],
 ]);
 
 // export const chainIDToPublicClient: Map<number, PublicClient> = new Map<
@@ -66,27 +66,27 @@ export const chainIDToRPCUrls: Map<number, `https://${string}` | `http://${strin
 // ]);
 
 export const chainIDToWalletClient: Map<number, WalletClient> = new Map<number, WalletClient>([
-	[
-		10,
-		createWalletClient({
-			chain: optimism,
-			transport: http('http://127.0.0.1:9545'),
-			// account: PUBLIC_KEY as `0x${string}`, // Ensuring correct type
-		}),
-	],
-	[
-		8453,
-		createWalletClient({
-			chain: base,
-			transport: http('http://127.0.0.1:9546'),
-			// account: PUBLIC_KEY as `0x${string}`,
-		}),
-	],
+    [
+        10,
+        createWalletClient({
+            chain: optimism,
+            transport: http('http://127.0.0.1:9545'),
+            // account: PUBLIC_KEY as `0x${string}`, // Ensuring correct type
+        }),
+    ],
+    [
+        8453,
+        createWalletClient({
+            chain: base,
+            transport: http('http://127.0.0.1:9546'),
+            // account: PUBLIC_KEY as `0x${string}`,
+        }),
+    ],
 ]);
 
 export const chainIDtoChain = new Map<number, Chain>([
-	[10, optimism],
-	[8453, base],
+    [10, optimism],
+    [8453, base],
 ]);
 
 export const DEPLOYER_ACCOUNT = privateKeyToAccount('0x');
@@ -96,3 +96,6 @@ export const ARBITRAGEOUR_ACCOUNT = privateKeyToAccount('0x');
 export const FAT_FINGER_ACCOUNT = privateKeyToAccount('0x');
 
 // EXTEND PUBLIC ACTIONS L2,
+export const BRIDGE_SWAP_CONTRACT = '0x4dc19314fc3019571e951e31dc514e0374d47d47';
+
+export const TOKEN_ADDRESS = '0x2b3B81c58962627EA5D4d992495B00F71Ac2381a';
